f5fd050abffd1e48b5b421cc13c269b048f19146
adao <adaojose7@gmail.com>
2019-10-27 21:37:23 +0000

    first

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+const logger = require('morgan');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+//app.use(logger('dev'));
+
+users= [];
+connections= [];
+
+
+server.listen(process.env.PORT || port);
+app.get('/', function (req, res) {
+    res.sendFile (__dirname + '/index.html')
+});
+
+io.sockets.on('connection', function(socket) {
+    connections.push(socket);//coloca as conexões no array
+    console.log('Connected: %s sockets connected', connections.length);//imprime o número de conexões do array
+   
+         //disconnect
+    socket.on('disconnect', function(data){
+        users.splice(users.indexOf(socket.username),1);
+        updateUsernames();
+        connections.splice(connections.indexOf(socket), 1);//remove do array
+        console.log('Disconnected: %s sockets connected', connections.length);//quando alguém disconeta imprime o nr de conexões
+    });
+
+    //send messages
+    socket.on('send message', function(data){
+        io.sockets.emit('new message', {msg: data, user: socket.username});
+
+    });
+    //New user
+    socket.on('new user', function(data, callback){
+       callback(true);
+       socket.username = data; //dados que foram recolhidos
+       users.push(socket.username);
+       updateUsernames();
+});
+    function updateUsernames(){
+        io.sockets.emit('get users', users); 
+    };
+});
+console.log(`Server running at  http://${hostname}:${port}/`);
